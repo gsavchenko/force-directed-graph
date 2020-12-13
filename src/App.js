e64bb3fbd164f15d56f1ad2388b293a8babe12ef
George Savchenko <george.savchenko@gmail.com>
2020-12-13 17:52:34 -0500

    setting up scene

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { Canvas } from 'react-three-fiber';
-import Box from './components/Box';
+import Patricle from './components/Patricle';
+import * as THREE from 'three';
+import { useRef } from "react";
 
 function App() {
+  const camera = useRef(new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ));
+
   return (
-    <Canvas style={{ height: '100vh', width: '100vw' }}>
-      <camera position={[0, 0, 5]} />
-      <Box />
+    <Canvas style={{ height: '100vh', width: '100vw' }} camera={camera}>
+      <Patricle />
     </Canvas>
   );
 }
