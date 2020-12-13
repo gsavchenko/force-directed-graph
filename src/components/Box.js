ce459409ed9bf95802375b27d6a11ba571108c99
George Savchenko <george.savchenko@gmail.com>
2020-12-13 17:53:08 -0500

    removing unused library

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { Canvas, useFrame } from "react-three-fiber";
+import { useFrame } from "react-three-fiber";
 import * as THREE from 'three';
 
 export default function Box() {
