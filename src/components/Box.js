25d4563fe763e0e428ac7ce7bd37d6a1b6f5f75b
George Savchenko <george.savchenko@gmail.com>
2020-12-13 17:09:20 -0500

    inital commit

---
+++ src/components/Box.js
@@ -0,0 +1,19 @@
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+import * as THREE from 'three';
+
+export default function Box() {
+  const cube = useRef();
+
+  useFrame(() => {
+    cube.current.rotation.x += 0.01;
+    cube.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={cube}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshBasicMaterial color={0x00ff00} />
+    </mesh>
+  )
+}
